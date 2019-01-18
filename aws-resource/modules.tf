4f3f74f52c5d99479a15496fddeb6af2696fb324
thepiger <beninipietro92@gmail.com>
2019-01-18 12:00:22 +0100

    Add files via upload

---
+++ aws-resource/modules.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "example" {
+  ami           = "${lookup(var.amis, var.region)}"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+output "id" {
+  value = "${aws_instance.example.id}"
+}
